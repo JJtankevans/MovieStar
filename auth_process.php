0bb766301b369cc1fe5048ebbb1d5431645e9c61
JJtankevans <v.oliveira042997@gmail.com>
2021-12-09 16:21:02 -0400

    Adicionando a função de login no sistema

--- auth_process.php
+++ auth_process.php
@@ -71,6 +71,20 @@
   
     } else if($type === "login") {
     
+      $email = filter_input(INPUT_POST, "email");
+      $password = filter_input(INPUT_POST, "password");
 
+      //Tenta autenticar usuário
+      if($userDao->authenticateUser($email, $password)){
+
+        $message->setMessage("Seja bem vindo!","success","editprofile.php");
+
+        //Redireciona o usuário, caso não consiga autenticar
+      }else{
+        $message->setMessage("Usuário e/ou senha incorretos","error","back");
+      }
+    } else {
+
+      $message->setMessage("Informações inválidas!","error","index.php");
     }
     
